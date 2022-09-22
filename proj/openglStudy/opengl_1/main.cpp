c9c6bf2b20650581e0ef63e8084a76d2b5680498
cCli8023 <420338169@qq.com>
2022-09-22 15:58:45 +0800

    纹理使用

--- proj/openglStudy/opengl_1/main.cpp
+++ proj/openglStudy/opengl_1/main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include "openglShader.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image/stb_image.h"
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -47,10 +50,10 @@ int main() {
     shader.start();
 
     float vertices[] = {
-        0.5f,  -0.5f, 0.0f,  // top right
-        0.5f, 0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f, 0.5f, 0.0f,  // bottom left
+        0.5f,   -0.5f,  0.0f,   2.0f, 0.0f,                         // top right
+        0.5f,   0.5f,   0.0f,   2.0f, 2.0f,                           // bottom right
+        -0.5f,  -0.5f,  0.0f,   0.0f, 0.0f,                         // bottom left
+        -0.5f,  0.5f,   0.0f,   0.0f, 2.0f                          // bottom left
     };
 
     unsigned int VBO;
@@ -74,14 +77,43 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
     //顶点数组对象需要的数据来自当前绑定到GL_ARRAY_BUFFER的buffer
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+  
 
     int nrAttributes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
     std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
+    //texture
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // 为当前绑定的纹理对象设置环绕、过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // 加载并生成纹理
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("E:\\lr\\ubuntu\\share\\lr\\learnOpengl\\proj\\openglStudy\\fourColor.bmp", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
 
     while (!glfwWindowShouldClose(window))
     {
